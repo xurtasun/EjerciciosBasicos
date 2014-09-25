91c0f42446a9398c850d732c86ad7e51c8f4a426
xurtasun <xavi.urta@gmail.com>
2014-09-25 13:58:05 +0200

    first commit

---
+++ src/main/java/edu/upc/eetac/dsa/xurtasun/EjerciciosBasicos/App.java
@@ -0,0 +1,65 @@
+package edu.upc.eetac.dsa.xurtasun.EjerciciosBasicos;
+import java.io.*;
+
+public class App 
+{
+	
+    public static void main( String[] args ) throws BigNumberException
+    {
+      File fichero = null;
+      Reader lectura = null;
+      BufferedReader tunel = null;
+      
+      try{
+    	  fichero = new File ("numeros.txt");
+    	  lectura = new FileReader (fichero);
+    	  tunel = new BufferedReader (lectura);
+    	  
+    	  String linea;
+    	  
+    	  while((linea=tunel.readLine())!=null)
+    	  {
+    		  String [] numeros = linea.split("-");
+    		  for (int i = 0;i < numeros.length;i++)
+    		  {
+    			  int entero = Integer.parseInt(numeros[i]);
+
+    			  if (entero>1000){
+    				  
+    				  throw new BigNumberException();
+    			  }
+    			  else
+	    			  {
+	    			  	System.out.println(numeros[i]);
+	    			  }
+    			  
+    			 
+
+    		  }
+    	  }
+      }
+      catch (BigNumberException b)
+	      {
+    	  	b.BigNumberException();
+	      }
+      catch(Exception e)
+	      {
+	    	  e.printStackTrace();
+	      }
+      finally
+      {
+    	  try{
+    		  if( null != lectura )
+	    		  {
+	    		  	lectura.close();
+	    		  }
+    		  }
+    	  catch (Exception e2)
+    	  		  {
+	    		 	e2.printStackTrace();
+	    		  }
+      }
+      
+      
+    }
+}
---
+++ src/main/java/edu/upc/eetac/dsa/xurtasun/EjerciciosBasicos/BigNumberException.java
@@ -0,0 +1,7 @@
+package edu.upc.eetac.dsa.xurtasun.EjerciciosBasicos;
+
+public class BigNumberException extends Exception{
+	public void BigNumberException() {
+		System.out.println ("Numero mayor a mil");
+		}
+}
